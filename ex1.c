82ab073fd28e8506b73e62dfcd72f05f538f0e8e
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:20:51 +0300

    first c program

---
+++ ex1.c
@@ -0,0 +1,13 @@
+// My first program
+
+#include <stdio.h>
+// tells the compiler to include std input output header file
+
+int main ()
+{
+	printf("Hello World\n");
+	// prints Hello world on the user screen
+	return 0;
+	// return the value zero to the OS
+}
+
