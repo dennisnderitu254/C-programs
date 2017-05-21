041438bb19f5567a5844c47bff7e7517c932d711
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:27:59 +0300

    Checking odd or even numbers

---
+++ ex5.c
@@ -0,0 +1,22 @@
+
+
+// Checking Odd or Even Numbers
+
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	printf("Enter an integer\n");
+	scanf("%d",&n);
+
+	// if n is completely divisible by 2 then prints even otherwise n is odd
+
+	if(n%2 == 0)
+		printf("Even\n");
+	else
+		printf("Odd\n");
+
+	return 0;
+}
