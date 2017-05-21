3136267d8cb27929cc45a641eec6e16479df65f2
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:37:14 +0300

    Calculating percentage using C

---
+++ ex12.c
@@ -0,0 +1,18 @@
+// Calculate percentage
+
+#include <stdio.h>
+
+void main()
+{
+	int s1,s2,s3,s4,s5,sum,total = 500;
+	float per;
+	printf("\nEnter marks of 5 subjects:\n");
+	scanf("%d%d%d%d%d",&s1,&s2,&s3,&s4,&s5);
+
+	sum = s1+s2+s3+s4+s5;
+	printf("\nSum : %d",sum);
+
+	per =(sum*100)/500;
+	// percentage formula
+	printf("\nPercentage: %f",per);
+}
