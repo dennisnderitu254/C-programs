f922911dcfc326239f832be5f9e741cbc0bd8346
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:56:03 +0300

    reversing numbers

--- ex25.c
+++ ex25.c
@@ -6,6 +6,23 @@ int main()
 {
 	int n,reverse=0;
 
-	printf("Enter a number to reverse:\n")
+	printf("Enter a number to reverse:\n");
+	scanf("%d",&n);
+
+	while(n!=0)
+	{
+		reverse=reverse*10;
+		reverse=reverse+n%10;
+		n=n/10;
+	}
+	/*
+	taking unit place digit of no and moving to reverse
+
+	dividing the no to discard unit place digit
+
+
+	*/
+	printf("Reverse of entered number is = %d\n", reverse);
+	return 0;
 
 }
