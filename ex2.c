d70865c7c97b4352c388edda16a8442081707fe8
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:24:37 +0300

    printing an integer

---
+++ ex2.c
@@ -0,0 +1,18 @@
+// Printing an integer
+
+#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	printf("Enter an integer\n");
+	scanf("%d",&a);
+	// takes an integer from user
+
+	printf("The Integer that you have entered is %d\n",a);
+
+	return 0;
+
+
+}
