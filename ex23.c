8f53c1c6001d6d7d863110ece087fb859b113f83
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:49:16 +0300

    Calculating the highest common factor and the least common multiple

---
+++ ex23.c
@@ -0,0 +1,56 @@
+// Highest common factors and least common multiple
+// HCF and LCM
+// using functions
+
+
+
+#include <stdio.h>
+
+long gcd(long,long);
+
+int main()
+{
+	long x,y,hcf,lcm;
+
+	printf("Enter two integers\n");
+	scanf("%ld%ld",&x,&y);
+
+	hcf = gcd(x,y);
+	lcm = (x*y)/hcf;
+
+	printf("Greatest common divisor of %ld and %ld = %ld\n",x,y,hcf);
+	printf("Least common multiple of %ld and %ld = %ld\n",x,y,lcm);
+
+	return 0;
+}
+
+/*
+
+if 1st no is 0 then 2nd number is the gcd
+make 2nd no 0 by subtracting smallest from largest and return 1st no as the GCD
+
+*/
+
+long gcd(long x,long y)
+{
+	if(x == 0){
+		return y;
+	}
+
+	while(y!= 0)
+	{
+		if(x>y)
+		{
+			x = x-y;
+		}
+		else
+		{
+			y=y-x;
+		}
+	}
+
+	return x;
+
+
+}
+
