194145423e6ac5e7fef3bb5c20c9106bd247c2d2
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:35:18 +0300

    Working with nested if statements

---
+++ ex11.c
@@ -0,0 +1,27 @@
+// Nested If Else
+
+#include <stdio.h>
+void main()
+{
+	int marks;
+	printf("Enter your marks: ");
+	scanf("%d",&marks);
+
+	if(marks>100)
+		// marks greater than 100
+		printf("Not valid marks!");
+	else if(marks>=80)
+		// marks between 80 & 99
+		printf("Your grade is A+");
+	else if(marks>=70)
+		// marks between 50 & 69
+		printf("Your grade is B");
+	else if(marks>=50)
+		printf("Your grade is C");
+	else if(marks>=35)
+		// marks between 35 & 49
+		printf("Your grade id D");
+	else
+		// marks less than 35
+		printf("Your grade is E");
+}
