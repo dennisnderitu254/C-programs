105635d4e0f159bf371888d99b5afe2e415b381c
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:42:00 +0300

    Calculating the Area of a square

---
+++ ex17.c
@@ -0,0 +1,16 @@
+// Area of Square
+
+#include <stdio.h>
+
+int main()
+{
+	int side,area;
+
+	printf("\nEnter the length of side:\n");
+	scanf("%d",&side);
+
+	area = side*side;
+	printf("\nArea of the Square: %d",area);
+
+	return 0;
+}
