32c94c0515722362b77da79511045aebdc587321
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:43:48 +0300

    Calculating the volume of a cylinder

---
+++ ex19.c
@@ -0,0 +1,18 @@
+// Volume of cylinder
+
+#include <stdio.h>
+
+void main()
+{
+	float vol,pie=3.14;
+	float r,h;
+
+	printf("ENTER THE VALUE OF RADIUS:-\n");
+	scanf("%f",&r);
+
+	printf("ENTER THE VALUE OF HEIGHT:-\n");
+	scanf("%f",&h);
+
+	vol = pie*r*r*h;
+	printf("VOLUME OF CYLINDER IS:%3.2f\n",vol);
+}
