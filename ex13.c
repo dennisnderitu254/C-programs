cf1c59c9e3a9ed17ef2ee6b190e2cb06b49af805
Dennis Nderitu <dknderitu@gmail.com>
2017-05-22 01:38:06 +0300

    Calculate gross salary

---
+++ ex13.c
@@ -0,0 +1,17 @@
+// Calculate Gross Salary
+
+#include <stdio.h>
+
+void main()
+{
+	int gross_salary,basic,da,ta;
+
+	printf("Enter basic salary:\n");
+	scanf("%d",&basic);
+
+	da = (10*basic)/100;
+	ta = (12*basic)/100;
+
+	gross_salary=basic+da+ta;
+	printf("\nGross salary: %d",gross_salary);
+}
