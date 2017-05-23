888dcd9086fc1339c37415e4d9de19cf440a7b11
Dennis Nderitu <dknderitu@gmail.com>
2017-05-23 16:13:23 +0300

    Working with the fibonacci series using loops

---
+++ ex35.c
@@ -0,0 +1,33 @@
+// Fibonacci Series Using Loop
+
+#include <stdio.h>
+
+int main()
+{
+	int n,first=0,second=1,next,c;
+
+	printf("Enter the number of terms\n");
+	scanf("%d",&n);
+
+	printf("First %d terms of Fibonacci series are:-\n",n);
+
+	for(c=0;c<n;c++)
+	{
+		if(c<=1)
+			next=c;
+		else
+		{
+			next=first+second;
+			first=second;
+			second=next;
+			/*
+			replace first no by second & second by addition of first & second
+			*/
+		}
+
+		printf("%d\n",next);
+	}
+
+	return 0;
+
+}
