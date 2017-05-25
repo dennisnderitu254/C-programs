c6cbe6e7bd101981c9fddc403df88c01918bcb3a
Dennis Nderitu <dknderitu@gmail.com>
2017-05-25 18:06:54 +0300

    printing the floyds triangle

---
+++ ex41.c
@@ -0,0 +1,24 @@
+// Print Floyd's Triangle
+
+#include <stdio.h>
+
+int main()
+{
+	int n,i,c,a=1;
+
+	printf("Enter the number of rows of Floyd's triangle to print\n");
+	scanf("%d",&n);
+
+	for(i=1;i<n;i++)
+	{
+		for(c=1;c<i;c++)
+		{
+			printf("%d",a);
+		}
+
+		printf("\n");
+		/* code*/
+	}
+
+	return 0;
+}
