4560b472d56f5cf63790436880534e62e9eb3fe7
Dennis Nderitu <dknderitu@gmail.com>
2017-06-06 00:13:11 +0300

    Subtracting Matrices using Arrays

---
+++ ex56.c
@@ -0,0 +1,41 @@
+// Subtract matrices
+
+#include <stdio.h>
+
+int main()
+{
+	int m,n,c,d,first[10][10],second[10][10],difference[10][10];
+
+	printf("Enter the number of rows and columns of matrix\n");
+	scanf("%d%d",&m,&n);
+
+	printf("Enter the Elements of the first matrix\n");
+
+	for(c=0;c<m;c++)
+	for(d=0;d<n;d++)
+	scanf("%d",&first[c][d]);
+
+    printf("Enter the Elements of the second matrix\n");
+
+    for(c=0;c<m;c++)
+	for(d=0;d<n;d++)
+	scanf("%d",&second[c][d]);
+    
+    for(c=0;c<m;c++)
+	for(d=0;d<n;d++)
+	difference[c][d]=first[c][d]-second[c][d];
+    // Subtract Matrices
+
+    printf("Difference of entered matrices:- \n");
+
+    for(c=0;c<m;c++)
+    {
+    	for(d=0;d<n;d++)
+    		printf("%d\t",difference[c][d]);
+    	    printf("\n");
+
+    }
+
+    return 0;
+
+}
