26668729ac54726ef93a19e7fb932f6efa7e9f20
Dennis Nderitu <dknderitu@gmail.com>
2017-06-06 00:24:33 +0300

    Computing the transpose of a matrix

---
+++ ex57.c
@@ -0,0 +1,43 @@
+// A program that Computes the transpose of a matrix
+
+#include <stdio.h>
+
+int main()
+{
+	int m,n,c,d,matrix[10][10],transpose[10][10];
+
+	printf("Enter the number of rows and columns of Matrix:\n");
+	scanf("%d%d",&m,&n);
+
+	printf("Enter the elements of the matrix\n");
+
+	for(c=0;c<m;c++)
+	{
+		for(d=0;d<n;d++)
+		{
+			scanf("%d",&matrix[c][d]);
+		}
+	}
+
+	for(c=0;c<m;c++)
+	{
+		for(d=0;d<n;d++)
+		{
+			transpose[d][c] = matrix[c][d];
+			/* transpose by interchanging rows and columns*/
+		}
+	}
+
+	printf("Transpose of entered matrix :- \n");
+
+	for(c=0;c<n;c++)
+	{
+		for(d=0;d<m;d++)
+		{
+			printf("%d\t",transpose[c][d]);
+		}
+
+		printf("\n");
+	}
+	return 0;
+}
