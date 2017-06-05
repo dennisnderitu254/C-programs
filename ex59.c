4fadd5ec4548f87eb59b25eff6be095c8ecaaeae
Dennis Nderitu <dknderitu@gmail.com>
2017-06-06 00:29:41 +0300

    A program that performs a linear search in arrays

---
+++ ex59.c
@@ -0,0 +1,40 @@
+/* This is a program that performs a linear search in Arrays*/
+
+
+#include <stdio.h>
+
+
+int main()
+{
+	int array[100],search,c,n;
+
+	printf("Enter the number of elements in array: \n");
+	scanf("%d",&n);
+
+	printf("Enter %d integers(s)\n",n);
+
+	for(c=0;c<n;c++)
+	scanf("%d",&array[c]);
+
+    printf("Enter the number to search\n");
+    scanf("%d",&search);
+
+    /* We keep on comparing each element with the element to search
+    until the desired element is found or list ends */
+
+    for(c=0;c<n;c++)
+    {
+    	if(array[c]==search)
+    	{
+    		// If the required element is found
+
+    		printf("%d is present at location %d.\n",search,c+1);
+    		break;
+    	}
+    }
+
+    if(c==n)
+    	printf("%d is not present in array.\n",search);
+
+    return 0;
+}
