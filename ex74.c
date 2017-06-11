8eda52dc3bac789eeea22272ec35442f3c2b0838
Dennis Nderitu <dknderitu@gmail.com>
2017-06-11 17:46:30 +0300

    Working on C substrings

---
+++ ex74.c
@@ -0,0 +1,55 @@
+// Writing a program that works on C substrings
+
+#include <stdio.h>
+#include <malloc.h>
+
+
+char* substring(char*,int,int);
+
+int main()
+{
+	char string[100],*pointer;
+	int position,length;
+
+	printf("Enter a string\n");
+	scanf("%s",string);
+
+	printf("Enter the position and length of substring\n");
+	scanf("%d%d",&position,&length);
+
+	pointer=substring(string,position,length);
+
+	printf("Required substring is \"%s\"\n",pointer);
+
+	free(pointer);
+
+	return 0;
+}
+
+/* C substring function:It returns a pointer to the substring */
+char*substring(char*string,int position,int length)
+{
+	char*pointer;
+	int c;
+
+	pointer = malloc(length+1);
+
+	if(pointer == NULL)
+	{
+		printf("Unable to allocate memory.\n");
+
+	}
+
+	for(c=0;c<position-1;c++)
+		string++;
+
+	for(c=0;c<length;c++)
+	{
+		*(pointer+c)=*string;
+		string++;
+	}
+
+	*(pointer+c) = '\0';
+
+	return pointer;
+}
