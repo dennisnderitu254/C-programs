d6f03d358351fe9223cb1f551c52c63f814bc86f
Dennis Nderitu <dknderitu@gmail.com>
2017-06-11 17:38:04 +0300

    This program determines the string length

---
+++ ex66.c
@@ -0,0 +1,19 @@
+// This is a C program that is going to be used to determine String Length
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+	char a[100];
+	int length;
+	printf("Enter a string to calculate it's length\n");
+	gets(a);
+
+	length=strlen(a);
+	/*strlen (string): function gives length of string */
+
+	printf("Length of entered string is = %d\n",length);
+	return 0;
+}
